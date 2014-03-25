3757ca63ae2c5f37f08b37761a10a2b46f44f953
milesstevenson <miles.d.stevenson@gmail.com>
2014-03-25 10:59:26 -0700

    Been a long time.

---
+++ chapter1/1-37.scm
@@ -0,0 +1,20 @@
+
+;; Exercise 1.37
+
+
+
+(define (cont-frac n d k)
+  (define (frac i)
+    (if (= k i)
+	(/ (n i) (d i))
+	(/ (n i) (+ (d i) (frac (+ i 1))))))
+  (frac 1))
+
+
+
+(define (cont-frac-it n d k)
+  (define (frac-itr sol i)
+    (if (= i 0)
+	sol
+	(frac-itr (/(n i) (+ (d i) sol)) (- i 1))))
+  (frac-itr 0 k))
