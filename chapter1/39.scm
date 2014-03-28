3160f7c8b52b10e49c03f24bb979645a4953d36a
Miles Stevenson <miles.d.stevenson@gmail.com>
2014-03-28 00:36:11 -0500

    fixed 1.39 to be more generalized and use lambda

--- chapter1/39.scm
+++ chapter1/39.scm
@@ -3,12 +3,17 @@
 (define (make-odd x)
   (- (* 2 x) 1))
 
+(define (cont-frac n d k)
+  (define (frac i)
+    (if (= i k)
+	(/ (n k) (d k))
+	(/ (n i) (- (d i) (frac (+ i 1))))))
+  (frac 1))
+
 (define (tan-cf x k)
-  (define (frac-i sol i)
-    (if (= i 0)
-	(/ sol x)
-	(frac-i (/ (square x) (- (make-odd i) sol)) (- i 1))))
-  (frac-i (square x) k))
+  (cont-frac (lambda (i) (if (= i 1) x (square x)))
+	     (lambda (i) (make-odd i))
+	     k))
 
 
 
