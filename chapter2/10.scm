9bab9bab0c90638983fdfbfa398709ba44a15c80
milesstevenson <miles.d.stevenson@gmail.com>
2014-04-24 10:53:03 -0700

    2.10 fixed

--- chapter2/10.scm
+++ chapter2/10.scm
@@ -5,9 +5,14 @@
   
 (define (upper-bound intrvl)
   (max (car intrvl) (cdr intrvl)))
+(define (width intrvl)
+  (/ (- (upper-bound intrvl) (lower-bound intrvl)) 2))
+
+(define (spans-zero? intrvl)
+  (= (width intrvl) 0))
 
 (define (div-interval x y)
-  (if (or (= (upper-bound y) 0) (= (lower-bound y) 0))
+  (if (spans-zero? y)
       (error "You can't divide by zero." y)
       (mul-interval x
                     (make-interval (/ 1.0 (upper-bound y))
