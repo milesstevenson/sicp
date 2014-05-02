b28a281be9334019d813f6743f3ee50e86e1b51e
milesstevenson <miles.d.stevenson@gmail.com>
2014-05-02 07:25:32 -0700

    2.13 done. bugs in 2.11 fixed

--- chapter2/13.scm
+++ chapter2/13.scm
@@ -1,32 +1,8 @@
-(define (make-interval a b) (cons a b))
-
-(define (lower-bound intrvl)
-  (min (car intrvl) (cdr intrvl)))
-  
-(define (upper-bound intrvl)
-  (max (car intrvl) (cdr intrvl)))
-
-;;;Original mul-interval
-(define (mul-interval x y)
-  (let ((p1 (* (lower-bound x) (lower-bound y)))
-	(p2 (* (lower-bound x) (upper-bound y)))
-	(p3 (* (upper-bound x) (lower-bound y)))
-	(p4 (* (upper-bound x) (upper-bound y))))
-    (make-interval (min p1 p2 p3 p4)
-		   (max p1 p2 p3 p4))))
-
-;;;By testing the signs of the endpoints of the intervals, it is
-;;;possible to break mul-interval into nine cases, only one of which
-;;;requires more than two multiplications.
 (define (mul-interval-rewrite x y)
   (let ((x-hi (upper-bound x))
         (x-lo (lower-bound x))
         (y-hi (upper-bound y))
         (y-lo (lower-bound y)))
-    ;;Cases handled:
-    ;; +,+  +,+
-    ;;      +,-
-    ;;      -,-
     (cond ((and (>= x-hi 0)
                 (>= x-lo 0)
                 (>= y-hi 0)
@@ -42,10 +18,6 @@
                 (<= y-hi 0)
                 (<= y-lo 0))
            (make-interval (*x-hi y-hi) (*x-hi y-lo )))
-          ;;Cases handled:
-           ;; +,-  +,+
-           ;;      +,-
-           ;;      -,-
           ((and (>= x-hi 0)
                 (<= x-lo 0)
                 (>= y-hi 0)
@@ -63,10 +35,6 @@
                 (<= y-hi 0)
                 (<= y-lo 0))
            (make-interval (* x-lo y-lo) (* x-hi y-lo)))
-           ;;Cases handled:
-           ;; -,-  +,+
-           ;;      +,-
-           ;;      -,-
           ((and (<= x-hi 0)
                 (<= x-lo 0)
                 (>= y-hi 0)
@@ -82,3 +50,62 @@
                 (<= y-hi 0)
                 (<= y-lo 0))
            (make-interval (* x-lo y-lo) (* x-hi y-hi))))))
+
+
+(define (make-center-width c w)
+  (make-interval (- c w) (+ c w)))
+
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
+
+(define (lower-bound intrvl)
+  (min (car intrvl) (cdr intrvl)))
+  
+(define (upper-bound intrvl)
+  (max (car intrvl) (cdr intrvl)))
+
+         
+(define (make-interval a b)
+  (cons a b))
+
+(define (make-center-percent c p)
+  (make-interval (+ (* c p) c) (- c (* c p))))
+
+(define (percent inv)
+  (/ (width inv) (center inv)))
+#|
+2 error> (percent a)
+
+;Value: .04285714285714283
+
+2 error> (percent b)
+
+;Value: .06666666666666662
+
+2 error> (percent (mul-interval-rewrite a b))
+
+;Value: .10921177587844252
+;--------------------------------------
+2 error> (percent c)
+
+;Value: .35000000000000003
+
+2 error> (percent d)
+
+;Value: .25
+
+2 error> (percent (mul-interval-rewrite c d))
+
+;Value: .5517241379310344
+
+|#
+
+
+;;; Tests above tell us that the APPROXIMATE percentage tolerance of the product
+;;; two intervals is (+ (percent a) (percent b)), where a and b are intervals with
+;;; a small percentage tolerance.
+(define (approx-mul-% a b)
+  (+ (percent a) (percent b)))
