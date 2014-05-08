f42bc31c9197bfcab00f36091bc56c5d13b6fcc6
milesstevenson <miles.d.stevenson@gmail.com>
2014-05-07 23:33:51 -0700

    a more elegant solution added for 2.28 along with my initial one

--- chapter2/28.scm
+++ chapter2/28.scm
@@ -13,7 +13,7 @@
 ;;; (fringe (list x x))
 ;;; (1 2 3 4 1 2 3 4)
 
-(define (fringe items)
+(define (fringe2 items)
   (define (fringe-begin items)
     (define (fringe-deep-iter mark)
       (cond ((equal? mark -1) '())
@@ -26,3 +26,12 @@
                      (fringe-deep-iter (- mark 1))))))
     (fringe-deep-iter (- (length items) 1)))
   (reverse (fringe-begin items)))
+
+;;; I'm quite late, but just adding this solution that looks a bit
+;;; more presentable and has a faster run-time, I'm sure.
+;;; That was some truly ugly shit I wrote as my first attempt above.
+(define (fringe tree)
+  (cond ((null? tree) '())
+        ((pair? tree) (append (fringe (car tree))
+                              (fringe (cdr tree))))
+        (else (list tree))))
