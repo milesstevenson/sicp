0ef7dfca6e5010b3ed4c48b6979bfb785aa7aec2
milesstevenson <miles.d.stevenson@gmail.com>
2014-05-28 08:49:07 -0700

    2.43 done

---
+++ chapter2/43.scm
@@ -0,0 +1,13 @@
+;;; The only thing that I can conclude from this exercise with certainty is that
+;;; everytime a single element of (enumerate-interval 1 board-size) is used in the
+;;; outermost flatmap, (queen-cols (- k 1)) must be re-evaluated. Without a doubt,
+;;; the running time of this new procedure is exponential.
+
+
+
+
+
+
+
+
+
