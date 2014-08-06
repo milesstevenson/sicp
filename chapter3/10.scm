8c9d83aed514bec6e0682869916db94154acff9f
Miles Stevenson <miles.d.stevenson@gmail.com>
2014-08-06 12:56:35 -0400

    modeling objects with state
    Probably not a proper solution, but gotta keep things moving.

--- chapter3/10.scm
+++ chapter3/10.scm
@@ -19,17 +19,19 @@
                     balance)
              "Insufficient funds"))) initial-amount)))
 
-;;; WARNING: This model below isn't correct. It is just my progress so far.
-;;;          Fixing everything when I return.
+;;; 
 ;;;
-;;; Here is my incorrect interpretation of the newly used evaluation model, evaluating
-;;; the following expression(s). This is very much the same object as in figure 3.7. To
-;;; understand why this modeling below is wrong, I will need to desugar figure 3.7 as well.
+;;; Taking a stab at this. I don't really know what I'm doing.
+;;; Others were referenced such as: 
 ;;;
-;;; A proper solution will come after work today or tomorrow morning.
+;;; https://github.com/skanev/playground/blob/master/scheme/sicp/03/10.scm
+;;; http://wqzhang.wordpress.com/2009/07/13/sicp-exercise-3-10/
+;;; https://github.com/bishboria/SICP/blob/master/exercises/3.10.lisp
+;;; and #racket on Freenode IRC.
 ;;;
+;;; Alright, here we go.
 ;;; (define W1 (make-withdraw 100))
-;;; more to come...
+;;; 
 
 ;     ____________________________________________________________________________________
 ;    |                                  global env                                        |
@@ -37,15 +39,76 @@
 ;    |   W1:+                                                                         |   |
 ;    |      |                                                                         |   |
 ;    |______|_________________________________________________________________________|___|
-;           |              ^                                                          |  ^
-;           |              |                                                         O O--
-;           |        ______|______                                                   |
-;           |   E1->|             |                                                  |
-;          O O----->| balance: 100|                                       parameters: initial-amount
-;          |        |_____________|                                       body: ((lambda (balance)
-;          |                                                                      ...) initial-amount)
-;          |
-;          |
-;         parameters: balance
-;         body: (lambda (amount)
-;                   ...)
+;           |              ^                                                          |   ^
+;           |              |                                                         O O--|
+;           |        ______|___________                                                   |
+;           |   E1->|                  |                                                  |
+;          O O----->|initial-amount:100|                              parameters: initial-amount
+;          |        |_____________ ____|                              body: ((lambda (balance)
+;          |                         ^                                           ...) initial-amount)
+;          |                         |
+;          |                         |
+;         parameters: amount         |
+;         body: (if (>= ...)         |
+;                   ...)             |
+;                       _____________|
+;                 E2-> | balance: 100|
+;                      |             |
+;                       -------------
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; (W1 50)
+;;;
+;     ____________________________________________________________________________________
+;    |                                  global env                                        |
+;    |   make-withdraw-desugared: -----------------------------------------------------   |
+;    |   W1:+                                                                         |   |
+;    |      |                                                                         |   |
+;    |______|_________________________________________________________________________|___|
+;           |              ^                                                          |   ^
+;           |              |                                                         O O--|
+;           |        ______|___________                                                   |
+;           |   E1->|                  |                                                  |
+;          O O----->|initial-amount:100|                              parameters: initial-amount
+;          |        |_____________ ____|                              body: ((lambda (balance)
+;          |                         ^                                           ...) initial-amount)
+;          |                         |
+;          |                         |
+;         parameters: amount         |
+;         body: (if (>= ...)         |
+;                   ...)             |
+;                       _____________|
+;                 E2-> | balance: 100|
+;                      |             |
+;                       -------------
+;                                 ^
+;                                 |
+;                       __________|__
+;                 E3-> |amount: 50   |
+;                      |             |
+;                       -------------
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; (define W2 (make-withdraw 100))
+;     ____________________________________________________________________________________
+;    |                                  global env                                        |
+;    |   make-withdraw-desugared: -----------------------------------------------------   |
+;    |   W1:+                                                                         |   |
+;    |   W2:|----------------------------------                                       |   |
+;    |______|_________________________________|_______________________________________|___|
+;           |              ^                  |                                       |   ^
+;           |              |                  |                                      O O--|
+;           |        ______|___________       |                                           |
+;           |   E1->|                  |      |                                           |
+;          O O----->|initial-amount:100|      |                       parameters: initial-amount
+;          |        |_____________ ____|      |                       body: ((lambda (balance)
+;          |                         ^        |            __________________    ...) initial-amount)
+;          |                         |        |      E3-> |                  |
+;          |<-----------------------|--------O O--------->|initial-amount:100| 
+;         parameters: amount         |                    |__________________|
+;         body: (if (>= ...)         |                     ^
+;                   ...)             |                     |
+;                       _____________|                     |  _____________ 
+;                 E2-> | balance: 50 |;                 E4-> | balance: 50 |
+;                      |_____________|                       |_____________|
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
