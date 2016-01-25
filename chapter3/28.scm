7db884102a8b55d20db276492e526909f7e16bcc
milesstevenson <miles.d.stevenson@gmail.com>
2016-01-24 21:53:59 -0600

    re-wrote exercise 3.28 to help get back into the swing of things and combined all of this propagation stuff into a utils.scm file

--- chapter3/28.scm
+++ chapter3/28.scm
@@ -1,17 +1,27 @@
 #lang racket
 (require r5rs/init)
+(require "utils.scm")
 
-(define (logical-or a b)
-  (cond ((and (= a 0) (= b 0)) 0)
-        (else 1)))
+(define (logical-or x y)
+  (if (and (= x 0) (= y 0))
+      0
+      1))
 
 (define (or-gate a1 a2 output)
   (define (or-action-procedure)
     (let ((new-value
-           (logical-or (get-signal a1) (get-signal a2))))
+          (logical-or (get-signal a1) (get-signal a2))))
       (after-delay or-gate-delay
                    (lambda ()
                      (set-signal! output new-value)))))
   (add-action! a1 or-action-procedure)
   (add-action! a2 or-action-procedure)
-  'ok)
+  'done)
+
+(define in1 (make-wire))
+(define in2 (make-wire))
+(define out (make-wire))
+(probe 'out out)
+(or-gate in1 in2 out)
+(set-signal! in1 1)
+(propagate)
---
+++ chapter3/utils.scm
@@ -0,0 +1,235 @@
+#lang racket
+(require r5rs/init)
+(provide (all-defined-out))
+
+;;; SECTION 3.3.2
+(define (front-ptr queue) (car queue))
+(define (rear-ptr queue) (cdr queue))
+(define (set-front-ptr! queue item) (set-car! queue item))
+(define (set-rear-ptr! queue item) (set-cdr! queue item))
+
+(define (empty-queue? queue) (null? (front-ptr queue)))
+(define (make-queue) (cons '() '()))
+
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      (error "FRONT called with an empty queue" queue)
+      (car (front-ptr queue))))
+
+(define (insert-queue! queue item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-queue? queue)
+           (set-front-ptr! queue new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue)
+          (else
+           (set-cdr! (rear-ptr queue) new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue)))) 
+
+(define (delete-queue! queue)
+  (cond ((empty-queue? queue)
+         (error "DELETE! called with an empty queue" queue))
+        (else
+         (set-front-ptr! queue (cdr (front-ptr queue)))
+         queue))) 
+
+;;; SECTION 3.3.4
+;;; Wire ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; A wire in our simulation will be a computational object with two local state
+;;; variables: a signal-value (initially taken to be 0) and a collection of 
+;;; action-procedures to be run when the signal changes value. We implement the
+;;; wire, using message-passing style, as a collection of local procedures
+;;; together with a dispatch procedure that selects the appropriate local 
+;;; operation.
+(define (make-wire)
+  (let ((signal-value 0) (action-procedures '()))
+    (define (set-my-signal! new-value)
+      (if (not (= signal-value new-value))
+          (begin (set! signal-value new-value)
+                 (call-each action-procedures))
+          'done))
+    
+    (define (accept-action-procedure! proc)
+      (set! action-procedures (cons proc action-procedures))
+      (proc))
+    
+    (define (dispatch m)
+      (cond ((eq? m 'get-signal) signal-value)
+            ((eq? m 'set-signal!) set-my-signal!)
+            ((eq? m 'add-action!) accept-action-procedure!)
+            (else (error "Unknown operation -- WIRE" m))))
+    dispatch))   
+  
+  ;; The local procedure set-my-signal! tests whether the new signal value
+  ;; changes the signal on the wire. If so, it runs each of the action
+  ;; procedures, using the following procedure call-each, which calls each of
+  ;; the items in a list of no-argument procedures.
+  (define (call-each procedures)
+    (if (null? procedures)
+	'done
+	(begin
+	  ((car procedures))
+	  (call-each (cdr procedures)))))
+
+;;; With the local dispatch procedure set up as specified, we can provide the
+;;; following procedures to access the local operations on wires.
+(define (get-signal wire)
+  (wire 'get-signal))
+
+(define (set-signal! wire new-value)
+  ((wire 'set-signal!) new-value))
+
+(define (add-action! wire action-procedure)
+  ((wire 'add-action!) action-procedure))
+
+;;; Agenda ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define (make-time-segment time queue)
+  (cons time queue))
+
+(define (segment-time s) (car s))
+
+(define (segment-queue s) (cdr s))
+
+(define (make-agenda) (list 0))
+
+(define (current-time agenda) (car agenda))
+
+(define (set-current-time! agenda time)
+  (set-car! agenda time))
+
+(define (segments agenda) (cdr agenda))
+
+(define (set-segments! agenda segments)
+  (set-cdr! agenda segments))
+
+(define (first-segment agenda) (car (segments agenda)))
+
+(define (rest-segments agenda) (cdr (segments agenda)))
+
+(define the-agenda (make-agenda))
+
+(define (empty-agenda? agenda)
+  (null? (segments agenda)))
+
+;;; To add an action to an agenda, we first check if the agenda is empty. If so,
+;;; we create a time segment for the actiona nd install this in the agenda.
+;;; Otherwise, we scan the agends, examining the time of each segment. If we
+;;; find a segment for out appointed time, we add the action to the associated
+;;; queue. If we reach a time later than the one to which we are appoited, we
+;;; insert a new time segment into the agenda just before it. If we reach the end
+;;; of the agenda, we must create a new time segment at the end.
+(define (add-to-agenda! time action agenda)
+  (define (belongs-before? segments)
+    (or (null? segments)
+        (< time (segment-time (car segments)))))
+  (define (make-new-time-segment time action)
+    (let ((q (make-queue)))
+      (insert-queue! q action)
+      (make-time-segment time q)))
+  (define (add-to-segments! segments)
+    (if (= (segment-time (car segments)) time)
+        (insert-queue! (segment-queue (car segments))
+                       action)
+        (let ((rest (cdr segments)))
+          (if (belongs-before? rest)
+              (set-cdr!
+               segments
+               (cons (make-new-time-segment time action)
+                     (cdr segments)))
+              (add-to-segments! rest)))))
+  (let ((segments (segments agenda)))
+    (if (belongs-before? segments)
+        (set-segments!
+         agenda
+         (cons (make-new-time-segment time action)
+               segments))
+        (add-to-segments! segments))))
+
+(define (remove-first-agenda-item! agenda)
+  (let ((q (segment-queue (first-segment agenda))))
+    (delete-queue! q)
+    (if (empty-queue? q)
+        (set-segments! agenda (rest-segments agenda)))))
+
+(define (first-agenda-item agenda)
+  (if (empty-agenda? agenda)
+      (error "Agenda is empty -- FIRST-AGENDA-ITEM")
+      (let ((first-seg (first-segment agenda)))
+        (set-current-time! agenda (segment-time first-seg))
+        (front-queue (segment-queue first-seg)))))
+ 
+(define (after-delay delay action)
+  (add-to-agenda! (+ delay (current-time the-agenda))
+                  action
+                  the-agenda))
+;;; The simulation is driven by propagate, which operates on the-agenda,
+;;; executing each procedure on the agenda in sequence. In general, as the
+;;; simulation runs, new items will be added to the agenda, and propagate will
+;;; continue the simulation as long as there are items on the agenda.
+(define (propagate)
+  (if (empty-agenda? the-agenda)
+      'done
+      (let ((first-item (first-agenda-item the-agenda)))
+        (first-item)
+        (remove-first-agenda-item! the-agenda)
+        (propagate))))
+;;; And-Gate ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; An and-gate is a little more complex. The action procedure must be run
+;;; if either of the inputs to the gate changes. It computes the logical-and
+;;; of the values of the signals on the input wires and sets up a change to the
+;;; new value to occur on the output wire after one and-gate delay.
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let ((new-value
+           (logical-and (get-signal a1) (get-signal a2))))
+      (after-delay and-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'ok)
+
+(define (logical-and x y)
+  (if (and (= x 1) (= y 1))
+      1
+      0))
+  
+;;; Inverter ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; need to implement and gate and or gate and inverter
+(define (inverter input output)
+  (define (invert-input)
+    (let ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! input invert-input)
+  'ok)
+(define (logical-not s)
+  (cond ((= s 0) 1)
+        ((= s 1) 0)
+        (else (error "Invalid signal" s))))
+
+;;; Half-Adder ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;(define (half-adder a b s c)
+;  (let ((d (make-wire)) (e (make-wire)))
+;    (or-gate a b d)
+;    (and-gate a b c)
+;    (inverter c e)
+;    (and-gate d e s)
+;    'ok))
+;;; Sample Simulation ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define (probe name wire)
+  (add-action! wire
+	       (lambda ()
+                 (newline)
+                 (display name)
+                 (display " ")
+                 (display (current-time the-agenda))
+                 (display " New-value = ")
+                 (display (get-signal wire)))))
+;: (define the-agenda (make-agenda))
+(define inverter-delay 2)
+(define and-gate-delay 3)
+(define or-gate-delay 5)
+;;; TODO
